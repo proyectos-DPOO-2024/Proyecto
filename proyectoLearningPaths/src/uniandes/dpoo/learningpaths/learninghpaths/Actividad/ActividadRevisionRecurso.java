21777e27fcb944750f94878dbf79cdcf90029a82
NicolasMarioRomero <157537981+NicolasMarioRomero@users.noreply.github.com>
2024-10-20 11:28:05 -0500

    Update ActividadRevisionRecurso.java

--- proyectoLearningPaths/src/uniandes/dpoo/learningpaths/learninghpaths/Actividad/ActividadRevisionRecurso.java
+++ proyectoLearningPaths/src/uniandes/dpoo/learningpaths/learninghpaths/Actividad/ActividadRevisionRecurso.java
@@ -1,4 +1,4 @@
-package uniandes.dpoo.learninghpaths.Actividad;
+package uniandes.dpoo.learningpaths.learninghpaths.Actividad;
 
 public class ActividadRevisionRecurso extends Actividad {
 	private String tipoRecurso;
