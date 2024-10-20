ee77176c8a6cb9711ed0c2fe7f718d31c4efdb3f
Particula173 <je.pulidog1@uniandes.edu.co>
2024-10-20 01:07:41 -0500

    avance

--- proyectoLearningPaths/src/uniandes/dpoo/learningpaths/usuarios/Estudiante.java
+++ proyectoLearningPaths/src/uniandes/dpoo/learningpaths/usuarios/Estudiante.java
@@ -11,7 +11,44 @@ public class Estudiante extends Usuario {
 		super(usuarioID, nombreUsuario, nombre, apellido, contraseña);
 		learningPathsInscritos = new LinkedList<String>();
 	}
+
+	public List<String> getLearningPathsInscritos() {
+		return learningPathsInscritos;
+	}
+
+	public void setLearningPathsInscritos(List<String> learningPathsInscritos) {
+		this.learningPathsInscritos = learningPathsInscritos;
+	}
 	
 	
+	public void inscribirLearningPath(LearningPath path) {
+	    if (!learningPathsInscritos.contains(path)) {
+	        learningPathsInscritos.add(path);
+	    }
+	}
 	
+	public void listarLearningPaths() {
+	    for (LearningPath path : learningPathsInscritos) {
+	        System.out.println(path.getTitulo());
+	    }
+	}
+	
+	public void verProgreso(LearningPath path) {
+	    if (learningPathsInscritos.contains(path)) {
+	        // Lógica para mostrar el progreso del estudiante en el Learning Path
+	        System.out.println("Progreso: " + path.getProgreso(this));
+	    } else {
+	        System.out.println("No estás inscrito en este Learning Path.");
+	    }
+	}
+
+	public void comenzarActividad(LearningPath path, Actividad actividad) {
+	    if (learningPathsInscritos.contains(path)) {
+	        // Lógica para permitir al estudiante comenzar una actividad
+	        actividad.iniciar();
+	    } else {
+	        System.out.println("Debes estar inscrito en el Learning Path para comenzar una actividad.");
+	    }
+	}
+
 }
--- proyectoLearningPaths/src/uniandes/dpoo/learningpaths/usuarios/Profesor.java
+++ proyectoLearningPaths/src/uniandes/dpoo/learningpaths/usuarios/Profesor.java
@@ -11,6 +11,35 @@ public class Profesor extends Usuario {
 		super(usuarioID, nombreUsuario, nombre, apellido, contraseña);
 		learnignPathsCreados = new LinkedList<String>();
 	}
+
+	public List<String> getLearnignPathsCreados() {
+		return learnignPathsCreados;
+	}
+
+	public void setLearnignPathsCreados(List<String> learnignPathsCreados) {
+		this.learnignPathsCreados = learnignPathsCreados;
+	}
 	
+	public void crearLearningPath(LearningPath path) {
+	    learnignPathsCreados.add(path);
+	}
+
+	public void listarLearningPathsCreados() {
+	    for (LearningPath path : learnignPathsCreados) {
+	        System.out.println(path.getTitulo());
+	    }
+	}
 	
+	public void verReseñas(Actividad actividad) {
+	    List<Reseña> reseñas = actividad.getReseñas();
+	    for (Reseña reseña : reseñas) {
+	        System.out.println(reseña.getTexto());
+	    }
+	}
+
+	public Actividad clonarActividad(Actividad actividadOriginal) {
+	    Actividad copia = new Actividad(actividadOriginal); // Constructor de copia
+	    return copia;
+	}
+
 }
