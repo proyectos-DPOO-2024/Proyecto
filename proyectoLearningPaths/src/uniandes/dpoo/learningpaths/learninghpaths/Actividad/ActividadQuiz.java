1dc4ec8ead4e3259727839741e70e0e7039a6b75
NicolasMarioRomero <157537981+NicolasMarioRomero@users.noreply.github.com>
2024-11-13 20:52:00 -0500

    Update ActividadQuiz.java

--- proyectoLearningPaths/src/uniandes/dpoo/learningpaths/learninghpaths/Actividad/ActividadQuiz.java
+++ proyectoLearningPaths/src/uniandes/dpoo/learningpaths/learninghpaths/Actividad/ActividadQuiz.java
@@ -6,22 +6,50 @@ import java.util.List;
 
 public class ActividadQuiz extends Actividad {
 	
-	private List<Pregunta> pregunta;
-
-    public ActividadQuiz(String titulo, String descripcion, String objetivo, String nivelDificultad, int duracion, Float calificacion) {
-
-        super(titulo, descripcion, objetivo, nivelDificultad, duracion, calificacion);
-        
-        this.setPregunta(new ArrayList<Pregunta>());
-        
-    }
-
-	public List<Pregunta> getPregunta() {
-		return pregunta;
+	private List<Pregunta> listaPreguntas;
+	private double calificacionMinima;
+	
+	public ActividadQuiz(String titulo, String descripcion, String objetivo, String nivelDificultad, int duracion, double calificacionMinima) {
+		super(titulo, descripcion, objetivo, nivelDificultad, duracion);
+		this.listaPreguntas = new ArrayList<>();
+		this.calificacionMinima = calificacionMinima;
 	}
-
-	public void setPregunta(List<Pregunta> pregunta) {
-		this.pregunta = pregunta;
+	
+	public List<Pregunta> getListaPreguntas() {
+		return listaPreguntas;
+	}
+	
+	public double getCalificacionMinima() {
+		return calificacionMinima;
+	}
+	
+	public void agregarPregunta(Pregunta pregunta) {
+		listaPreguntas.add(pregunta);
+	}
+	
+	public void eliminarPregunta(int indice) {
+		if (indice >= 0 && indice < listaPreguntas.size()) {
+            listaPreguntas.remove(indice);
+		} else {
+			System.out.println("Índice fuera de rango, no se pudo eliminar la pregunta.");
+		}
+	}
+	
+	public void realizar() {
+        System.out.println("Participando en el quiz...");
+        this.marcarCompletada();
+    }
+	
+	public void participarQuiz() {
+		for (int i = 0; i <= listaPreguntas.size(); i++) {
+			System.out.println(listaPreguntas.get(i).getEnunciado());
+			System.out.println(listaPreguntas.get(i).getOpciones());
+		}
+		realizar();
+	}
+	
+	public Actividad clonar() {
+		return new ActividadQuiz(getTitulo(), getDescripcion(), getObjetivo(), getDificultad(), getDuracion(), calificacionMinima);
 	}
 
 }
