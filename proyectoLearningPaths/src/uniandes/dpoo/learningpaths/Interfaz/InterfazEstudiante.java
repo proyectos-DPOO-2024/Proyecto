e306f4519a0684123a1c8e3c3876a1e989df9ba4
NicolasMarioRomero <157537981+NicolasMarioRomero@users.noreply.github.com>
2024-12-07 17:26:36 -0500

    Interfaz estudiante agregada

---
+++ proyectoLearningPaths/src/uniandes/dpoo/learningpaths/Interfaz/InterfazEstudiante.java
@@ -0,0 +1,20 @@
+package uniandes.dpoo.learningpaths.Interfaz;
+
+import java.awt.EventQueue;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import uniandes.dpoo.learningpaths.persistencias.PersistenciaActividades;
+import uniandes.dpoo.learningpaths.persistencias.PersistenciaLearningPaths;
+import uniandes.dpoo.learningpaths.persistencias.PersistenciaResenia;
+import uniandes.dpoo.learningpaths.persistencias.PersistenciaUsuarios;
+import uniandes.dpoo.learningpaths.usuarios.Estudiante;
+import uniandes.dpoo.learningpaths.usuarios.Profesor;
+import uniandes.dpoo.learningpaths.usuarios.Usuario;
+
+public class InterfazEstudiante {
+
+}
